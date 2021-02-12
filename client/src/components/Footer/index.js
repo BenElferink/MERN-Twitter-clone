10e43a3d40a46a23d6f2ec1482a9399dcc63de9f
Ben Elferink <ben.elferink@icloud.com>
2021-02-12 12:27:46 +0200

    fotter message

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,5 +1,10 @@
 import styles from './index.module.css';
 
 export default function Footer() {
-  return <div className={styles.component}>FOOTER</div>;
+  return (
+    <div className={styles.component}>
+      Disclaimer: this clone is not associated with Twitter! All accounts & data are fictive, but
+      remain in a database.
+    </div>
+  );
 }
