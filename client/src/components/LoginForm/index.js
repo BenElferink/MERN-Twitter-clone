398945817e603b0d576b9fda3504c6197f13aa10
Ben Elferink <ben.elferink@icloud.com>
2021-02-15 14:38:50 +0200

    added async login function

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,18 +1,45 @@
 import { useState } from 'react';
 import styles from './index.module.css';
+import axios from '../../api';
 import Input from '../Input';
 import Button from '../Button';
+import Loading from '../Loading';
 
 export default function LoginForm({ onPage, clickRegister }) {
+  const [submitting, setSubmitting] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setSubmitting(true);
+    const formData = {
+      username,
+      password,
+    };
+
+    try {
+      const response = await axios.post('/users/login', formData);
+      console.log(`✅ ${response.status} ${response.statusText}`);
+      setSubmitting(false);
+
+      console.log(response);
+      alert('logged in successfully');
+    } catch (error) {
+      console.error('❌', error);
+      setSubmitting(false);
+
+      if (error.response.status === 400) {
+        alert(error.response.data.message);
+      } else {
+        alert('an unnexpected error occurred');
+      }
+    }
   };
 
   return (
     <form className={styles[onPage]} onSubmit={handleSubmit}>
+      {/* form inputs */}
       <Input
         label='Username'
         name='username'
@@ -27,8 +54,15 @@ export default function LoginForm({ onPage, clickRegister }) {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Button text='Log in' design={onPage === 'public' ? 'outlined' : 'filled'} type='submit' />
 
+      {/* submit */}
+      {submitting ? (
+        <Loading />
+      ) : (
+        <Button text='Log in' design={onPage === 'public' ? 'outlined' : 'filled'} type='submit' />
+      )}
+
+      {/* register (conditional) */}
       {onPage === 'login' && (
         <button className={styles.register} onClick={clickRegister}>
           Sign up for Twitter
